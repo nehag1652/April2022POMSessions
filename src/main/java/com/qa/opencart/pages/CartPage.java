f3a6324a44c5af7545e141e46c37d3e5be55b8f2
Neha Garg <nehag1652@gmail.com>
2023-03-27 19:04:05 -0400

    changes as per reviewd by Archana

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -4,7 +4,13 @@ public class CartPage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		By locator;
+		
 		System.out.println("We are on Cart Page");
+		
+		return;
+		
 	}
 
 }
