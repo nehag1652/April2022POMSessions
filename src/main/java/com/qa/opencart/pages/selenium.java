9ceab23ad5a55ab2d123f2dd9ae5fd745f2efa73
Neha Garg <nehag1652@gmail.com>
2023-03-27 23:30:50 -0400

    Added Selenium page

---
+++ src/main/java/com/qa/opencart/pages/selenium.java
@@ -0,0 +1,15 @@
+package com.qa.opencart.pages;
+
+public class selenium {
+
+	public static void main(String[] args) {
+		
+		int i=10;
+		int j=4;
+		System.out.println("This is class selenium");
+		
+		
+		
+	}
+
+}
